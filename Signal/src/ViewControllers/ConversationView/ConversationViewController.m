a3891f505ef60fa1b358a5fbac68f8e0b1386b1e
Matthew Chen <charlesmchen@gmail.com>
2017-10-16 12:36:04 -0400

    Reduce relayout and reload churn; respond to dynamic type changes.
    // FREEBIE

--- Signal/src/ViewControllers/ConversationView/ConversationViewController.m
+++ Signal/src/ViewControllers/ConversationView/ConversationViewController.m
@@ -1350,7 +1350,6 @@ typedef NS_ENUM(NSInteger, MessagesRangeSizeMode) {
 
 #pragma mark - Dynamic Text
 
-// TODO: Fix this.
 /**
  Called whenever the user manually changes the dynamic type options inside Settings.
 
@@ -2768,6 +2767,11 @@ typedef NS_ENUM(NSInteger, MessagesRangeSizeMode) {
         // may need to extend the mapping's contents to reflect the current
         // range.
         [self updateMessageMappingRangeOptions:MessagesRangeSizeMode_Normal];
+        // Calling resetContentAndLayout is a bit expensive.
+        // Since by definition this won't affect any cells in the previous
+        // range, it should be sufficient to call invalidateLayout.
+        //
+        // TODO: Investigate whether we can just call invalidateLayout.
         [self resetContentAndLayout];
         return;
     }
